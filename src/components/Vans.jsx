2a4f1e249c7774325810361798446ee080237c97
ozgeahras <ozgeahras@gmail.com>
2023-07-28 08:45:47 +0300

    added Vans component

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+
 export default function Vans() {
-  return <h1>Vans buraya</h1>;
+  const [allVans, setAllVans] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/vans");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const jsonData = await response.json();
+        console.log(jsonData.vans);
+        setAllVans(jsonData.vans);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const vanElements = allVans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
+
+  return (
+    <div className="van-list-container">
+      <div className="van-list">{vanElements}</div>
+    </div>
+  );
 }
