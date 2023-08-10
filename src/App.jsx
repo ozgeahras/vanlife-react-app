b4d97f58ba964151fda5a3207fe888591be37dda
ozgeahras <ozgeahras@gmail.com>
2023-08-10 16:28:00 +0300

    added hostLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
 import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
 import "./server";
 import "./App.css";
 
@@ -19,9 +20,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,3 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export default function Dashboard() {
-  return <h1>Dashboard Here</h1>;
+  return (
+    <>
+      <h1>Dashboard goes here</h1>
+      <Outlet />
+    </>
+  );
 }
