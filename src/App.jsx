fba555c7b4688d67dfc88e9f7bcb5822133c5ffc
ozgeahras <ozgeahras@gmail.com>
2023-08-11 14:23:51 +0300

    added host - van components and css

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
+import HostVanDetail from "./pages/Host/HostVanDetail";
+import HostVans from "./pages/Host/HostVans";
 import "./server";
 import "./App.css";
 
@@ -20,11 +22,12 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
         </Route>
       </Routes>
