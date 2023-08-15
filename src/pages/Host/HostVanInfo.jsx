5074d929891228b21ffd1d41de3c6872222c2cd7
ozgeahras <ozgeahras@gmail.com>
2023-08-15 15:42:45 +0300

    updated detail components

--- src/pages/Host/HostVanInfo.jsx
+++ src/pages/Host/HostVanInfo.jsx
@@ -1,3 +1,12 @@
+import { useOutletContext } from "react-router-dom";
 export default function HostVanInfo() {
-  return <h2>Detailed info goes here</h2>;
+  const currentVan = useOutletContext();
+  return (
+    <section>
+      <h4>Name: {currentVan.name}</h4>
+      <h4>Category: {currentVan.type}</h4>
+      <h4>Description: {currentVan.description}</h4>
+      <h4>Visibility: public</h4>
+    </section>
+  );
 }
--- src/pages/Host/HostVanPhotos.jsx
+++ src/pages/Host/HostVanPhotos.jsx
@@ -1,3 +1,5 @@
+import { useOutletContext } from "react-router-dom";
 export default function HostVanPhotos() {
-  return <h2>Photos go here</h2>;
+  const currentVan = useOutletContext();
+  return <img src={currentVan.imageUrl} className="host-van-detail-image" />;
 }
